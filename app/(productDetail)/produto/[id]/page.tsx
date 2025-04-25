5c09dfd6ab9d8b35db246b36e48485b873025951
geancanova <geancanovawt@gmail.com>
2025-04-25 18:36:51 -0300

    fix: params as Promise

--- app/(productDetail)/produto/[id]/page.tsx
+++ app/(productDetail)/produto/[id]/page.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default async function ProductPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
   const product = await fetchProduct(id);
